d4aec94f7b9a75925a563b56fa563f2bc2a3ddbd
ki <redcedar1@naver.com>
2024-09-09 21:08:17 +0900

    map updating api path configured

--- meetin/mysite/updatelocation/urls.py
+++ meetin/mysite/updatelocation/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('update-location/', views.update_location_view, name='update_location_view'),
     path('nearby-users/', views.nearby_users_view, name='nearby_users_view'),
+    path('api/update-location/', views.update_location, name='update_location'),
+    path('api/nearby-users/', views.nearby_users, name='nearby_users'),
 ]
