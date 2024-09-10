7f87430fea9df1e6f1a4b578c8780953817a39b2
ki <redcedar1@naver.com>
2024-09-10 13:36:34 +0900

    kakao login rest_api key, uri configured

--- meetin/mysite/common/views/common_views.py
+++ meetin/mysite/common/views/common_views.py
@@ -54,16 +54,16 @@ def kakaologin(request):
 
 
 def kakaoLoginLogic(request):
-    _restApiKey = '60010e5242c371826d538b43def648c3'  # 입력필요
-    _redirectUrl = 'http://127.0.0.1:8000/kakaoLoginLogicRedirect'
+    _restApiKey = 'bf24f04aa4c3aabf2644d871e7673f65'  # 입력필요
+    _redirectUrl = 'http://ec2-52-79-77-37.ap-northeast-2.compute.amazonaws.com:8080/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
     return redirect(_url)
 
 
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code']
-    _restApiKey = '60010e5242c371826d538b43def648c3'  # 입력필요
-    _redirect_uri = 'http://127.0.0.1:8000/kakaoLoginLogicRedirect'
+    _restApiKey = 'bf24f04aa4c3aabf2644d871e7673f65'  # 입력필요
+    _redirect_uri = 'http://ec2-52-79-77-37.ap-northeast-2.compute.amazonaws.com:8080/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url)
     _result = _res.json()
